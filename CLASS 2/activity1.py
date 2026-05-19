be195d492917d2f6430969099bab2c9f27df40a8
aryanthehuman2990-art <aryanthuman2990@gmail.com>
2026-05-19 13:21:01 +0530

    completed

---
+++ CLASS 2/activity1.py
@@ -0,0 +1,15 @@
+name="Aryan"
+age=13
+weight=120.00
+student=True
+print(name)
+print(age)
+print(weight)
+print(student)
+print (type(name))
+print (type(age))
+print (type(weight))
+print (type(student))
+print(int(weight))
+print (type(int(weight)))
+print(age+weight)
