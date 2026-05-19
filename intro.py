e5399aa4a84b412990990c554803bfc3e7531b51
aryanthehuman2990-art <aryanthuman2990@gmail.com>
2026-05-19 12:41:57 +0530

    git config --list

---
+++ intro.py
@@ -0,0 +1,10 @@
+#write a sentence
+print ("my name is aryan")
+#write a number
+print (1)
+#write a number and sentence together
+print("my name is aryan", (1))
+print("my name is aryan", 1)
+#write a sentence with an empty line in between
+print("\nmyname is aryan")
+
