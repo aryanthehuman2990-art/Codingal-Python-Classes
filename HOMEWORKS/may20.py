622f1c49df66ec87fc70ffabe8484aa4e511ccc0
aryanthehuman2990-art <aryanthuman2990@gmail.com>
2026-05-23 13:20:07 +0530

    homweork for may 20

--- HOMEWORKS/may20.py
+++ HOMEWORKS/may20.py
@@ -3,3 +3,6 @@ absent= int(input("Enter the amount of days you have been absent: "))
 total= present + absent
 percentage= (present/total)*100
 print("Your attendance percentage is: ", percentage, "%")
+if percentage>=75:
+    print("You are allowed to sit in the exam") 
+else:    print("You are not allowed to sit in the exam")
