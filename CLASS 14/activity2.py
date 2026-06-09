a7c585bf5d3af1f94fcb026cca1061f8d986a7c1
aryanthehuman2990-art <aryanthuman2990@gmail.com>
2026-06-09 13:37:09 +0530

    completed

--- CLASS 14/activity2.py
+++ CLASS 14/activity2.py
@@ -1,6 +1,7 @@
 #  Rotating and Adjusting Brightness
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 my_image=cv2.imread("C:/Users/ARYAN CHHABRA/Downloads/website.jpg")
 changer= cv2.cvtColor(my_image, cv2.COLOR_BGR2RGB)
 # Rotate the image by 45 degrees around its center
@@ -14,4 +15,6 @@ war=cv2.warpAffine(my_image, rot, (h,w))
 plt.imshow(war)
 plt.show()
 # Increase brightness by adding 50 to all pixel values
+brightness= np.ones(my_image.shape, dtype="uint8")*50
 # Use cv2.add to avoid negative values or overflow
+bad=cv2.add()
